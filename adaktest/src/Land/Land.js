f3138084b3e86309087d50bb84f96e62a815a24b
kamyab78 <amirhosseinghassemi78@yahoo.com>
2020-08-17 12:45:16 +0430

    Add initial project

---
+++ adaktest/src/Land/Land.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import Websocket from 'react-websocket';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import './Land.css'
+
+class Land extends Component{
+render(){
+    return(
+        <div className='main row'>
+            
+        </div>
+    )
+}
+}
+export default Land
---
+++ adaktest/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Land from './Land/Land'
+const App = () => {
+    return (
+        <Router>
+        <Switch>
+           
+                <Route exact path="/" component={Land}/>
+              
+        </Switch>           
+        </Router>
+ 
+ 
+    )
+}
+
+ReactDOM.render(
+    <App/>, document.querySelector('#root')
+)

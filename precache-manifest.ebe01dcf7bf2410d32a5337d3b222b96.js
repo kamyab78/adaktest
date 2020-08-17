e1f33cd8c9c183d49064f5c40485dfd94428f369
kamyab78 <amirhosseinghassemi78@yahoo.com>
2020-08-17 13:19:01 +0430

    Updates

---
+++ precache-manifest.ebe01dcf7bf2410d32a5337d3b222b96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ecad0eefd87d59eb2688da16962ec2",
+    "url": "/adaktest/index.html"
+  },
+  {
+    "revision": "f855f1f25b77f8d01348",
+    "url": "/adaktest/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d2d79a4a62f121d8182b",
+    "url": "/adaktest/static/css/main.f9f33dd3.chunk.css"
+  },
+  {
+    "revision": "f855f1f25b77f8d01348",
+    "url": "/adaktest/static/js/2.ba887031.chunk.js"
+  },
+  {
+    "revision": "f1ff62136e241c7f3e8294ce2bdd524c",
+    "url": "/adaktest/static/js/2.ba887031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d79a4a62f121d8182b",
+    "url": "/adaktest/static/js/main.6ac3005d.chunk.js"
+  },
+  {
+    "revision": "47c81a22cbaca1231a23",
+    "url": "/adaktest/static/js/runtime-main.64acb4fa.js"
+  }
+]);

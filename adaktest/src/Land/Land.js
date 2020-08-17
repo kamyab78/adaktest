9babdee39f2dfa22360a4772c26c94b4115f3237
kamyab78 <amirhosseinghassemi78@yahoo.com>
2020-08-18 01:32:18 +0430

    Add git.io config and basic setup

--- adaktest/src/Land/Land.js
+++ adaktest/src/Land/Land.js
@@ -1,16 +1,33 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Websocket from 'react-websocket';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 import $ from 'jquery';
 import './Land.css'
+var ws = ''
+class Land extends Component {
+    componentDidMount() {
 
-class Land extends Component{
-render(){
-    return(
-        <div className='main row'>
-            
-        </div>
-    )
-}
+
+        $(document).ready(function () {
+            $("#connect").click(function () {
+                ws = new WebSocket('wss://echo.websocket.org/')
+                ws.onopen = function () {
+                    console.log("connect")
+                };
+            })
+          
+        })
+    }
+    render() {
+        return (
+            <div className='main row'>
+                <div>
+                  <Button ariant="primary" id='connect'></Button>   
+                </div>
+               
+            </div>
+        )
+    }
 }
 export default Land

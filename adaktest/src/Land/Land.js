eff947d0ceecd5548a06d4f305db6d0113a76a26
kamyab78 <amirhosseinghassemi78@yahoo.com>
2020-08-18 04:15:06 +0430

    Add particle and btn and text landing

--- adaktest/src/Land/Land.js
+++ adaktest/src/Land/Land.js
@@ -4,7 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 import $ from 'jquery';
 import './Land.css'
+import Particles from 'react-particles-js';
 var ws = ''
+
 class Land extends Component {
     componentDidMount() {
 
@@ -16,16 +18,54 @@ class Land extends Component {
                     console.log("connect")
                 };
             })
-          
+            $(".btn_start").click(function () {
+                var elmnt = document.getElementById("webs_page");
+                elmnt.scrollIntoView({ behavior: 'smooth' });
+            })
         })
     }
     render() {
         return (
-            <div className='main row'>
-                <div>
-                  <Button ariant="primary" id='connect'></Button>   
+            <div >
+                <div className='main'>
+                    <div className='main_black'>
+                        <Particles height='100vh'
+
+                            params={{
+
+                                "particles": {
+                                    "number": {
+                                        "value": 150
+                                    },
+                                    "size": {
+                                        "value": 5
+                                    },
+                                    "color": {
+                                        "value": "#ff0000"
+                                    },
+                                },
+                                "interactivity": {
+                                    "events": {
+                                        "onHover": {
+                                            "enable": true,
+                                            "mode": "repulse"
+                                        }
+                                    }
+                                }
+                            }}
+                        >
+
+                        </Particles>
+                        <div className="txt-container " >
+
+                            <div className="txt-content col-sm-12 col-lg-8 col-xl-8 col-md-8" style={{ display: "block" }}>
+                                Welcome To Adak Test Project
+           </div>
+                            <Button variant="danger" className="btn_start">Get Start</Button>
+                        </div>
+                    </div>
                 </div>
-               
+                <div className='webs' id='webs_page'></div>
             </div>
         )
     }
